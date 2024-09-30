b857f8ce16eb14fca6953570a3fb2c1bed19de7f
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2024-09-30 18:10:30 -0400

    finished Spring

--- src/Spring.ts
+++ src/Spring.ts
@@ -29,11 +29,27 @@ export class Spring extends DrawnObjectBase {
     public override get w() {return super.w;}  
     public override set w(v : number) {
         //=== YOUR CODE HERE ===
+        if (!(v === this._w)) {
+            if (v <= this.wConfig.max && v >= this.wConfig.min) {
+                this._w = v; 
+                this._wConfig = SizeConfig.elastic(v);
+                this.damageAll();
+            }
+        }
+
     }
 
     public override get h() {return super.h;}
     public override set h(v : number) {
         //=== YOUR CODE HERE ===
+        if (!(v === this._h)) {
+            if (v <= this.hConfig.max && v >= this.hConfig.min) {
+                this._h = v; 
+                this._hConfig = SizeConfig.elastic(v);
+                this.damageAll();
+            }
+
+        }
     }
 
 
